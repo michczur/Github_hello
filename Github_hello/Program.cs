8d20737afceb3d4d53a167f5c782f037120d8491
MIchał Czuryłło <michczur@gmail.com>
2022-01-16 14:28:34 +0100

    Zadanie 1

--- Github_hello/Program.cs
+++ Github_hello/Program.cs
@@ -4,9 +4,23 @@ namespace Github_hello
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Price_list(double price, int lines)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= lines; i++)
+            {
+                Console.WriteLine("Liczba sztuk:" + i + " łaczna cena: " + price * i + " zł");
+            }
+
         }
+        static void Main(string[] args)
+            {
+                Console.WriteLine("podaj cene");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("podaj ilosc produktow");
+                int lines = Convert.ToInt32(Console.ReadLine());
+                Price_list(price, lines);
+
+            }
     }
 }
+
