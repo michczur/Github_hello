4605616c33b03a9e806ce6f60ea3ef04e53d4f36
MIchał Czuryłło <michczur@gmail.com>
2022-01-16 13:55:56 +0100

    "Witaj E00"

--- Github_hello/Program.cs
+++ Github_hello/Program.cs
@@ -6,7 +6,7 @@ namespace Github_hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Witaj E00 :-)!");
         }
     }
 }
